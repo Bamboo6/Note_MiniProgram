f1e8117eba4c78345ffa908f21009fc1d7f720e4
Bamboo6 <dream1718@foxmail.com>
2018-05-17 20:52:23 +0800

    fix bugs & insert time to sql

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,70 +1,86 @@
 //index.js
-var newnote
-var nickName
-var imageurl
-
+var util = require('../../utils/util.js');  
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    newnote:"",//留言信息
+    nickName: "",//用户昵称
+    imageurl: "",//头像地址
+    time: "",
+    authorized: false//是否获取用户信息授权
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this
-    //
-    wx.login({
-      success: function(){
-        wx.getUserInfo({
-          success: function(res){
-            nickName = res.userInfo.nickName
-            imageurl = res.userInfo.avatarUrl
-          }
-        })
-      }
-    }),
+    var time = util.formatTime(new Date());
+    this.setData({
+      time: time
+    });  
+
     wx.request({
       url: 'https://llm2isay.qcloud.la/list.php',
-        success: function(res){
-          that.setData({
+        success: (res)=>{
+          this.setData({
             array: res.data
           })
         }
     })
   },
 
-  //
+  //输入留言
   confirm: function(e){
-    newnote = e.detail.value
+    this.setData({newNote:e.detail.value})
   },
-  //
+
+  //提交留言
   click: function(e){
-    var that = this
+    
+    if(!this.data.newNote.trim()){
+      return
+    }
     wx.request({
       url: 'https://llm2isay.qcloud.la/note.php',
       data: {
-        name: nickName,
-        content: newnote,
-        imageurl: imageurl
+        name: this.data.nickName,
+        content: this.data.newNote,
+        imageurl: this.data.imageUrl,
+        // time:(new Date()).toLocaleString()
+        time: this.data.time
       },
     header:{
       'content-type':'application/x-www-form-urlencoded'
     },
-    success: function(res){
+    success: (res)=>{
       wx.request({
         url: 'https://llm2isay.qcloud.la/list.php',
-        success: function(res){
-          that.setData({
-            array: res.data
+        success: (res)=>{
+          this.setData({
+            array: res.data,
+            newNote: ""
           })
         }
       })
     }
     })
+    
+  },
+  getUserInfo:function(e){
+    this.setData({
+      nickName: JSON.parse(e.detail.rawData).nickName,
+      imageUrl: JSON.parse(e.detail.rawData).avatarUrl,
+      authorized: true 
+    })
   }
+  // gettime: function (e) {
+  //   var time = util.formatTime(new Date());  
+  //   this.setData({
+  //     time: time
+  //   });  
+  // }
 })
--- server/note.php
+++ server/note.php
@@ -3,6 +3,11 @@ include("conn.php");
 $name = $_GET['name'];
 $content = $_GET['content'];
 $imageurl = $_GET['imageurl'];
-$sql = "INSERT INTO `note` (`name`,`content`,`imageurl`)VALUES ('{$name}','{$content}','{$imageurl}')";
-mysql_query($sql);
+$time = $_GET['time'];
+$sql = "INSERT INTO `note` (`name`,`content`,`imageurl`,`time`)VALUES ('{$name}','{$content}','{$imageurl}','{$time}')";
+if(mysql_query($sql)){
+  echo "留言成功";
+}else{
+  echo "留言失败";
+}
 ?>
